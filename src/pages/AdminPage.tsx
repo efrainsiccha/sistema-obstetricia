5fc1f2e9890327053859629132a6187498e9632f
Efrain Siccha <163354946+efrainsiccha@users.noreply.github.com>
2025-11-13 00:01:18 -0500

    diseño en bloques

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -221,7 +221,7 @@ export default function AdminPage() {
         </Button>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
         <StatsCard
           title="Usuarios"
           value={usuarios.length}
